47e0466fd140c7c07a2f44009cf0d5ff2b6d1f8f
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-30 12:53:08 -0500

    [Docs] Add logo

--- src/lib.rs
+++ src/lib.rs
@@ -291,6 +291,7 @@
 //! [logical disjunction]:   https://en.wikipedia.org/wiki/Logical_disjunction
 
 #![deny(missing_docs)]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/nvzqz/does_impl/assets/logo.svg?sanitize=true")]
 
 #[doc(hidden)]
 pub extern crate core as _core;
